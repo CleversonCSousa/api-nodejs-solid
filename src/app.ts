a0b8e06516e7d880e37ee51c51526b8169f3494d
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-04 12:37:05 -0300

    Criado os relacionamentos entre tabelas

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,2 @@
 import fastify from 'fastify';
-import { PrismaClient } from '../generated/prisma/client';
-
-const prisma = new PrismaClient();
 export const app = fastify();
-
-prisma.user.create({
-    data: {
-        name: 'Cleverson Sousa',
-        email: 'cleversonsousa.git@gmail.com'
-    }
-});
