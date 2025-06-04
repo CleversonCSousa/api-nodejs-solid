fcf16788b34ed67810bba3c821eeadd957703ab9
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-04 11:56:12 -0300

    Instalação do Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify';
+import { PrismaClient } from '../generated/prisma/client';
 
+const prisma = new PrismaClient();
 export const app = fastify();
+
+prisma.user.create({
+    data: {
+        name: 'Cleverson Sousa',
+        email: 'cleversonsousa.git@gmail.com'
+    }
+});
