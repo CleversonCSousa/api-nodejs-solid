493388ec56ec56700e126a577742b63e69038d24
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-06-08 13:37:11 -0300

    fix:change import directory

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
 import { z }  from 'zod';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { RegisterUseCase } from '@/use-cases/register';
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
