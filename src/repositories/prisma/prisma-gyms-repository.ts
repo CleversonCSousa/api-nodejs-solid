d5228beabc5edb2fbc51388598cbacc228694276
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-08-13 15:02:28 -0300

    feat: add prisma gyms repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { Prisma, Gym } from 'generated/prisma';
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository';
+import { prismaClient } from '@/lib/prisma/prismaClient';
+
+export class PrismaGymsRepository implements GymsRepository {
+    async findById(id: string) {
+        const gym = await prismaClient.gym.findUnique({
+            where: {
+                id
+            }
+        });
+
+        return gym;
+    }
+    async findManyNearby({ latitude, longitude }: FindManyNearbyParams){
+        const gyms = await prismaClient.$queryRaw<Gym[]>`
+            SELECT * from gyms
+            WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+        `;
+
+        return gyms;
+    }
+    async searchMany(query: string, page: number) {
+        const gyms = await prismaClient.gym.findMany({
+            where: {
+                title: {
+                    contains: query
+                }
+            },
+            take: 20,
+            skip: (page - 1) * 20
+        });
+
+        return gyms;
+    }
+    async create(data: Prisma.GymCreateInput) {
+        const gym = await prismaClient.gym.create({
+            data
+        });
+
+        return gym;
+    }
+}
