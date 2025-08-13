1b90c6d952cde6c654c5245890e1d4fadb155354
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-08-13 16:15:14 -0300

    fix: remove the find many method from prism check in repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,7 +2,6 @@ import { CheckIn, Prisma } from 'generated/prisma';
 
 export interface CheckInsRepository {
     findById(id: string) : Promise<CheckIn | null>;
-    findMany() : Promise<CheckIn[]>;
     create(data: Prisma.CheckInUncheckedCreateInput) : Promise<CheckIn>;
     findManyByUserId(userId: string, page: number) : Promise<CheckIn[]>;
     countByUserId(userId: string) : Promise<number>;
