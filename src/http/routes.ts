d1b51d8139a39dce8eee40e58002eee065d74f3e
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-08-20 14:23:39 -0300

    completing the profile controller and add authentication middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
 import { profile } from './controllers/profile';
+import { verfiyJWT } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register);
     app.post('/sessions', authenticate);
     
-    app.get('/me', profile);
+    // Authenticated route
+    app.get('/me', { onRequest: [ verfiyJWT ]} , profile);
 }
 
